a1d027e5c06ae64d5e60288fe17fc1e7050bfd2e
Eyorokumo <eyorokorumodicksonemmanuel@gmail.com>
2024-03-09 12:06:03 +0100

    add

---
+++ javacript/js/youtube.js
@@ -0,0 +1,28 @@
+
+// alert("hello world")
+
+
+// document.getElementById("btn").onclick = function(){
+//        document.getElementById("confirm").innerHTML = "Order Placed. check email for confirmation";
+//        document.getElementById("btn").style.display = "none";
+// }
+
+// document.getElementById('btn').onclick = function(){
+//     document.getElementById('Get Started').innerHTML = 'Checked out our Courses for Free.Click for free';
+//     document.getElementById('btn').style.backgroundColor= 'green'
+// }
+
+// var userName = prompt();
+// console.log(userName)
+// alert(userName)
+
+// var age = 50;
+// // age = 50;
+// var newAge = age;
+
+// var message = 'newAge = ' + newAge; // variable plus expressions
+// console.log(message);
+
+
+
+
